a1f3b812207bd013a76dc81cbaf39732c66a1719
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-05-10 18:12:36 -0400

    Know what is Frame class

--- MyTankGame/src/com/alex/tank/TankFrame.java
+++ MyTankGame/src/com/alex/tank/TankFrame.java
@@ -1,10 +1,22 @@
 package com.alex.tank;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class TankFrame {
     public static void main(String[] args) {
         Frame f = new Frame();
+        f.setSize(800, 600);
+        f.setResizable(false);
+        f.setTitle("Tank Game");
         f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }

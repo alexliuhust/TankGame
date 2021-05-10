749871473eda3338dec5219bd2cf5f14b76ab547
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-05-10 18:01:13 -0400

    First

---
+++ MyTankGame/src/com/alex/tank/TankFrame.java
@@ -0,0 +1,10 @@
+package com.alex.tank;
+
+import java.awt.*;
+
+public class TankFrame {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setVisible(true);
+    }
+}

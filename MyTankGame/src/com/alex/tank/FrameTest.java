7625b65fe1e81b14813d7ad435ab8258cb40973a
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-05-10 18:30:24 -0400

    Extends Frame class

---
+++ MyTankGame/src/com/alex/tank/FrameTest.java
@@ -0,0 +1,9 @@
+package com.alex.tank;
+
+import java.awt.*;
+
+public class FrameTest {
+    public static void main(String[] args) {
+        TankFrame tf = new TankFrame();
+    }
+}
--- MyTankGame/src/com/alex/tank/TankFrame.java
+++ MyTankGame/src/com/alex/tank/TankFrame.java
@@ -4,19 +4,24 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class TankFrame {
-    public static void main(String[] args) {
-        Frame f = new Frame();
-        f.setSize(800, 600);
-        f.setResizable(false);
-        f.setTitle("Tank Game");
-        f.setVisible(true);
+public class TankFrame extends Frame {
 
-        f.addWindowListener(new WindowAdapter() {
+    public TankFrame() {
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.setTitle("Tank Game");
+        this.setVisible(true);
+
+        this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        
     }
 }

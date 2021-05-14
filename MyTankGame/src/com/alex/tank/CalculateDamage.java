7583d59b43ca1a854c80509f5be15bc01a22d09c
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-05-13 20:29:25 -0400

    Calculate damages according to the types of bullets

--- MyTankGame/src/com/alex/tank/CalculateDamage.java
+++ MyTankGame/src/com/alex/tank/CalculateDamage.java
@@ -1,18 +1,38 @@
 package com.alex.tank;
 
+import java.util.Random;
+
 public class CalculateDamage {
 
     public static int bulletDamage(Bullet b) {
+        Random rand = new Random();
+        int random_number = rand.nextInt(100);
+
         if (b.type.equals("AP")) {
-            return 100;
+            int penetrateProb = 100 - b.flyingTime * 3;
+            if (random_number <= penetrateProb) {
+                return 600;
+            } else {
+                return 10;
+            }
         }
 
         if (b.type.equals("AT")) {
-            return 200;
+            int penetrateProb = 70;
+            if (random_number <= penetrateProb) {
+                return 450;
+            } else {
+                return 200;
+            }
         }
 
         if (b.type.equals("HE")) {
-            return 300;
+            int penetrateProb = 10 - b.flyingTime;
+            if (random_number <= penetrateProb) {
+                return 800;
+            } else {
+                return 200;
+            }
         }
 
         return 0;

4dbb7b628060ebaba0cfc904b9208a4b8a985092
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-06-01 00:20:53 -0400

    Refactor codes for Melee Effect

--- ChessGame/src/effect/MeleeEffect.java
+++ ChessGame/src/effect/MeleeEffect.java
@@ -7,17 +7,12 @@ import java.awt.*;
 
 public class MeleeEffect extends Effect {
 
-    public int x1;
-    public int y1;
-    public int x2;
-    public int y2;
+    private Arm attacker, defender;
 
     public MeleeEffect(Arm attacker, Arm defender, Color effectColor) {
         super();
-        this.x1 = attacker.central()[0];
-        this.y1 = attacker.central()[1];
-        this.x2 = defender.central()[0];
-        this.y2 = defender.central()[1];
+        this.attacker = attacker;
+        this.defender = defender;
         this.lastTime = 10;
         this.effectColor = effectColor;
     }
@@ -32,6 +27,10 @@ public class MeleeEffect extends Effect {
         Color originalColor = g.getColor();
         g.setColor(effectColor);
 
+        int x1 = attacker.central()[0];
+        int y1 = attacker.central()[1];
+        int x2 = defender.central()[0];
+        int y2 = defender.central()[1];
         g.drawLine(x1, y1, x2, y2);
         g.drawLine(x1 - 1, y1 - 1, x2 - 1, y2 - 1);
         g.drawLine(x1 + 1, y1 - 1, x2 + 1, y2 - 1);

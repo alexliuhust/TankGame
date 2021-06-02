1f43f006d41519be2f9f43c4f45423fc7db88779
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-06-02 17:11:02 -0400

    Dynamic melee effect

--- ChessGame/src/effect/MeleeEffect.java
+++ ChessGame/src/effect/MeleeEffect.java
@@ -21,6 +21,7 @@ public class MeleeEffect extends Effect {
     public void paint(Graphics g, BattleField bf) {
         if (this.lastTime <= 0) {
             bf.effects.remove(this);
+            return;
         }
         this.lastTime--;
 
@@ -29,8 +30,14 @@ public class MeleeEffect extends Effect {
 
         int x1 = attacker.central()[0];
         int y1 = attacker.central()[1];
-        int x2 = defender.central()[0];
-        int y2 = defender.central()[1];
+        int x2_target = defender.central()[0];
+        int y2_target = defender.central()[1];
+
+        int[] lengths = {2,2,4,4,6,6,4,4,2,2,2};
+        int relativeLength = lengths[lastTime];
+        int x2 = x1 + (x2_target - x1) * relativeLength / 6;
+        int y2 = y1 + (y2_target - y1) * relativeLength / 6;
+
         g.drawLine(x1, y1, x2, y2);
         g.drawLine(x1 - 1, y1 - 1, x2 - 1, y2 - 1);
         g.drawLine(x1 + 1, y1 - 1, x2 + 1, y2 - 1);

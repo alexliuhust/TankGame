73b95297fc1c2a99b32cc192254628265b080219
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-06-03 16:52:33 -0400

    Big Hit has damage

--- ChessGame/src/effect/BigHitEffect.java
+++ ChessGame/src/effect/BigHitEffect.java
@@ -32,14 +32,30 @@ public class BigHitEffect extends Effect {
         int width = 120 - 4 * lastTime;
         int x = central_x - width / 2;
         int y = central_y - width / 2;
+        this.paintOval(g, x, y, width);
+        this.enemiesGetHit(x, y, width);
+    }
 
+    private void paintOval(Graphics g, int x, int y, int R) {
         Color originalColor = g.getColor();
         g.setColor(effectColor);
-
-        g.drawOval(x, y, width, width);
-        g.drawOval(x + 1, y + 1, width - 2, width - 2);
-        g.drawOval(x + 2, y + 2, width - 4, width - 4);
-
+        g.drawOval(x, y, R, R);
+        g.drawOval(x + 1, y + 1, R - 2, R - 2);
+        g.drawOval(x + 2, y + 2, R - 4, R - 4);
         g.setColor(originalColor);
+    }
+
+    private void enemiesGetHit(int x, int y, int R) {
+        Rectangle r1 = new Rectangle(x, y, R, R);
+        for (Arm arm : enemies) {
+            Rectangle r2 = new Rectangle(arm.leftTop()[0], arm.leftTop()[1], Arm.Width, Arm.Height);
+            if (r1.intersects(r2)) {
+                int damage = 6;
+                arm.hp -= damage * (100 - arm.getArmor()) / 100;
+                if (arm.hp < 0) {
+                    arm.alive = false;
+                }
+            }
+        }
     }
 }
